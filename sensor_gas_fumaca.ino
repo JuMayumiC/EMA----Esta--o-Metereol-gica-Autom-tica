b1ca05d1c4b3106424fdcb7f7204faa0de3f370d
Julia <julia.mayumi.costa@escola.pr.gov.br>
2024-07-05 10:35:10 -0300

    Atualizações no código do sensor de gás

--- sensor_gas_fumaca.ino
+++ sensor_gas_fumaca.ino
@@ -2,9 +2,7 @@
 /* Definição dos pinos */
 #define MQ_analogico A0
 #define MQ_digital 4
-#define Buzzer 5
-#define Led_Verde 6
-#define Led_Vermelho 7
+
 /* Variáveis */
 int valor_analogico;
 int valor_digital;
@@ -15,9 +13,7 @@ Serial.begin(9600);
 /* Configura os pinos de entrada e saída */
 pinMode(MQ_analogico, INPUT);
 pinMode(MQ_digital, INPUT);
-pinMode(Buzzer, OUTPUT);
-pinMode(Led_Verde, OUTPUT);
-pinMode(Led_Vermelho, OUTPUT);
+
 }/* end setup */
 void loop() {
 valor_analogico = analogRead(MQ_analogico);
@@ -28,16 +24,12 @@ Serial.print(" || ");
 if (valor_digital == 0)
 {
  Serial .println("GÁS DETECTADO !!!");
- digitalWrite(Led_Verde, LOW);
- digitalWrite(Led_Vermelho, HIGH);
- tone(Buzzer, 262, 250);
+
 }/* end if */
 else
 {
  Serial.println("GAS AUSENTE !!!");
- digitalWrite(Led_Verde, HIGH);
- digitalWrite(Led_Vermelho, LOW);
- noTone(Buzzer);
+
 }/* end else */
 delay(500);
 }/* end loop */
