569d6c2e7eb71e33834fe7eeb9ca514aed680836
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-07-26 10:23:01 -0300

    Update sensor_umidade_temperatura.ino

--- sensor_umidade_temperatura.ino
+++ sensor_umidade_temperatura.ino
@@ -1,36 +1,41 @@
 #include <Adafruit_Sensor.h>
-/* Programa: Sensor de Temperatura e Umidade DHT11 */
-/* Inclui a biblioteca de controle do sensor */
 #include "DHT.h"
-/* Cria o objeto dht para o sensor DHT11 no pino A0 do Arduino
-*/
+
+/* Define o tipo de sensor e o pino ao qual está conectado */
 DHT dht(A0, DHT11);
-void setup()
-{
-/* Inicia a comunicação serial; */
-Serial.begin(9600);
-/* Inicia o objeto do sensor */
-dht.begin();
+
+void setup() {
+  /* Inicia a comunicação serial */
+  Serial.begin(9600);
+  
+  /* Inicia o sensor */
+  dht.begin();
 }
-void loop()
-{
-/* Realiza a leitura do sensor e armazena os dados nas
-variáveis h e t */
-float h = dht.readHumidity();
-float t = dht.readTemperature();
-/* Faz os dados de umidade ficarem sem casas decimais */
-String H = String(h,0);
-/* Faz os dados da temperatura ficarem com uma casa decimal */
-String T = String(t,1);
-/* Escreve no monitor serial os valores lidos de Umidade e
-Temperatura */
-Serial.print("Umidade: ");
-Serial.print(H);
-Serial.print("%");
-Serial.print("\t");
-Serial.print("Temperatura: ");
-Serial.print(T);
-Serial.println("°C");
-/* Aguarda 2 segundos para a nova leitura */
-delay(2000);
+
+void loop() {
+  /* Realiza a leitura do sensor e armazena os dados nas variáveis h e t */
+  float h = dht.readHumidity(); // Lê a umidade
+  float t = dht.readTemperature(); // Lê a temperatura em Celsius
+  
+  /* Verifica se a leitura do sensor foi bem-sucedida */
+  if (isnan(h) || isnan(t)) {
+    Serial.println("Falha ao ler o sensor DHT!");
+    return;
+  }
+  
+  /* Formata os dados de umidade e temperatura para impressão no monitor serial */
+  String H = String(h, 0); // Umidade sem casas decimais
+  String T = String(t, 1); // Temperatura com uma casa decimal
+  
+  /* Escreve no monitor serial os valores lidos de Umidade e Temperatura */
+  Serial.print("Umidade: ");
+  Serial.print(H);
+  Serial.print("%");
+  Serial.print("\t");
+  Serial.print("Temperatura: ");
+  Serial.print(T);
+  Serial.println("°C");
+  
+  /* Aguarda 2 segundos para a próxima leitura */
+  delay(2000);
 }
