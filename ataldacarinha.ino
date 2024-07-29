f3c6e0fec727d13f9d61e9cb1e36549928d2fb24
Julia Mayumi <julia.mayumi.costa@escola.pr.gov.br>
2024-07-29 10:09:57 -0300

    Update ataldacarinha.ino

--- ataldacarinha.ino
+++ ataldacarinha.ino
@@ -3,6 +3,29 @@
 // Inicializa o LCD com os números de pinos corretos.
 LiquidCrystal lcd(12, 11, 5, 4, 3, 2);
 
+byte onefourthChar[] = {
+  B00000, 
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B11111,
+  B11111
+};
+
+byte onefourthCharFlipped[] = {
+  B11111,
+  B11111,
+  B00000, 
+  B00000,
+  B00000,
+  B00000,
+  B00000,
+  B00000
+
+};
+
 byte fullChar[] = {
   B11111, // 11111 em binário
   B11111, // 11111 em binário
@@ -68,7 +91,10 @@ void setup() {
   lcd.createChar(1, mirroredChar);          // Carrega o caractere espelhado horizontalmente
   lcd.createChar(2, triangleCharFlipped);   // Carrega o caractere original espelhado verticalmente
   lcd.createChar(3, mirroredCharFlipped);   // Carrega o caractere espelhado horizontal e verticalmente
-  lcd.createChar(4, fullChar);        
+  lcd.createChar(4, fullChar); 
+  lcd.createChar(5, onefourthCharFlipped);
+  lcd.createChar(6, onefourthChar);
+  
 }
 
 void loop() {
@@ -111,4 +137,44 @@ void loop() {
   
 
   delay(2000); // Espera 2 segundos
+
+   lcd.clear();
+  // Exibe o primeiro conjunto de caracteres na primeira linha
+  lcd.setCursor(3, 0);
+  lcd.write(byte(6)); // Caractere original
+  lcd.setCursor(4, 0);
+  lcd.write(byte(6)); // Caractere espelhado horizontalmente
+
+  // Exibe o segundo conjunto de caracteres na primeira linha com espaçamento de 6 caracteres
+  lcd.setCursor(11, 0);
+  lcd.write(byte(6)); // Caractere original
+  lcd.setCursor(12, 0);
+  lcd.write(byte(6)); // Caractere espelhado horizontalmente
+
+  // Exibe o primeiro conjunto de caracteres na segunda linha
+  lcd.setCursor(3, 1);
+  lcd.write(byte(5)); // Caractere original espelhado verticalmente
+  lcd.setCursor(4, 1);
+  lcd.write(byte(5)); // Caractere espelhado horizontal e verticalmente
+
+  // Exibe o segundo conjunto de caracteres na segunda linha com espaçamento de 6 caracteres
+  lcd.setCursor(11, 1);
+  lcd.write(byte(5)); // Caractere original espelhado verticalmente
+  lcd.setCursor(12, 1);
+  lcd.write(byte(5)); // Caractere espelhado horizontal e verticalmente
+  
+  // Exibe o primeiro conjunto de caracteres na segunda linha
+  lcd.setCursor(6, 1);
+  lcd.write(byte(2)); // Caractere original espelhado verticalmente
+  lcd.setCursor(9, 1);
+  lcd.write(byte(3)); // Caractere espelhado horizontal e verticalmente
+  
+   // Exibe o primeiro conjunto de caracteres na segunda linha
+  lcd.setCursor(7, 1);
+  lcd.write(byte(4)); // Caractere original espelhado verticalmente
+  lcd.setCursor(8, 1);
+  lcd.write(byte(4)); // Caractere espelhado horizontal e verticalmente
+  
+   
+  delay(1000); // Espera 2 segundos
 }
